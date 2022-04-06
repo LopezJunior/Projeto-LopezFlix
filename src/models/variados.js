897e298c0a300a2e3d221fd7a2e1a4654fe41d88
Maurício Júnior <68245295+LopezJunior@users.noreply.github.com>
2022-04-06 20:11:28 -0300

    Add files via upload

---
+++ src/models/variados.js
@@ -0,0 +1,52 @@
+import Sequelize from 'sequelize'
+import {
+    connection
+} from '../database/connection.js'
+
+export const variados = connection.define('variados', {
+
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+    },
+    nome: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+
+    },
+    ano: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+
+    },
+    img: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+    },
+
+    descricao: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+
+    },
+
+    diretores: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+
+    },
+
+    iframe: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+
+    },
+
+}, {
+    freezeTableName: true,
+    createdAt: false,
+    updatedAt: false,
+    timestamps: false
+})
